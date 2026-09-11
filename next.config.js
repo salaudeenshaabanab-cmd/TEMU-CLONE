836ea0128b02b54c545180bd778355ec4e8203fa
salaudeenshaabanab-cmd <salaudeenshaaban.ab@gmail.com>
2026-09-11 15:38:55 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,26 +1,9 @@
-{
-"compilerOptions": {
-"lib": ["dom", "dom.iterable", "esnext"],
-"allowJs": true,
-"skipLibCheck": true,
-"strict": true,
-"noEmit": true,
-"esModuleInterop": true,
-"module": "esnext",
-"moduleResolution": "bundler",
-"resolveJsonModule": true,
-"isolatedModules": true,
-"jsx": "preserve",
-"incremental": true,
-"plugins": [
-{
-"name": "next"
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['images.unsplash.com'],
+  },
 }
-],
-"paths": {
-"@/": ["./"]
-}
-},
-"include": ["next-env.d.ts", "/*.ts", "/.tsx", ".next/types/**/.ts"],
-"exclude": ["node_modules"]
-}
+
+module.exports = nextConfig
